bb9113a82cca0e49dc67d5a0b5fe580184e5c059
Nico Pierson <nicogpt@gmail.com>
2021-07-16 22:11:10 -0700

    css styles for sign and sign up in the same modal

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -22,6 +22,7 @@ function LoginForm() {
   };
 
   return (
+    <>
     <form onSubmit={handleSubmit} className={styles.form}>
       <h2 className={styles.form__title}>Sign In</h2>
       <ul className={styles.form__errors}>
@@ -49,8 +50,49 @@ function LoginForm() {
           placeholder='Password'
         />
       </div>
-      <button type='submit'>Log In</button>
+      <button type='submit' className={styles.form__submit_btn}>Log In</button>
     </form>
+
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <h2 className={styles.signup_form__title}>Sign Up</h2>
+      <ul className={styles.signup_form__errors}>
+        {errors.map((error, idx) => (
+          <li key={idx}>{error}</li>
+        ))}
+      </ul>
+      <div className={styles.signup_form__input_field}>
+        <i className='fa fa-user'></i>
+        <input
+          type='text'
+          value={credential}
+          onChange={(e) => setCredential(e.target.value)}
+          required
+          placeholder='Username'
+        />
+      </div>
+      <div className={styles.signup_form__input_field}>
+        <i className='fa fa-envelope'></i>
+        <input
+          type='text'
+          value={credential}
+          onChange={(e) => setCredential(e.target.value)}
+          required
+          placeholder='Email'
+        />
+      </div>
+      <div className={styles.signup_form__input_field}>
+        <i className='fas fa-lock'></i>
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          placeholder='Password'
+        />
+      </div>
+      <button type='submit' className={styles.signup_form__submit_btn}>Sign Up</button>
+    </form>
+    </>
   );
 }
 
