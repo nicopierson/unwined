f1c98183d2324123fb69c1fd864d878be68f5adb
Nico Pierson <nicogpt@gmail.com>
2021-07-19 17:48:48 -0700

    normalized data and added seed data to wines, wineries, and wine types

--- backend/assets/form_data.js
+++ backend/assets/form_data.js
@@ -1,19 +1,164 @@
-// import winesJSON from './wine-data-set.json';
+const fs = require("fs");
+
+
+function writeJSONFile (obj, filename) {
+  
+  fs.writeFile(filename, JSON.stringify(obj, null, 4), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    };
+    console.log("File has been created");
+  });
+}
+
+// normalize data
+function normalizeJSON (objJSON) {
+  const normalizedObj = objJSON.reduce((obj, entry, i) => {
+    obj[i] = entry;
+    return obj;
+  }, {});
+
+  return normalizedObj;
+}
+
+// normalize data
+function makeWineryJSON (objJSON) {
+
+  const winerySet = new Set();
+  const wineries = [];
+  objJSON.forEach(({country, winery}, i) => {
+    
+    if (winerySet.has(winery)) {
+      // do nothing
+    } else {
+      winerySet.add(winery);
+      const newObj = {
+        name: winery,
+        country,
+        ownerId: 1,
+        // index: i,
+      };
+      wineries.push(newObj);
+    }
+  });
+
+  return wineries;
+}
+
+// normalize data: with keys of name, country and ownerId
+// make owner id 1
+// keys of winery and country in the wine json
+function normalizeWineryJSON (objJSON) {
+  let wineryObj = {};
+  const winerySet = new Set();
+  objJSON.forEach(({country, winery}, i) => {
+    if (winerySet.has(winery)) {
+
+    } else {
+      winerySet.add(winery);
+      wineryObj[winery] = {
+        name: winery,
+        country,
+        ownerId: 1,
+        index: i,
+      };
+    }
+  });
+
+  return wineryObj;
+}
+
+function makeWineTypesJSON (objJSON) {
+  const wineSet = new Set();
+  const objArray = [];
+  objJSON.forEach(({variety}, i) => {
+    if (wineSet.has(variety)) {
+
+    } else {
+      wineSet.add(variety);
+      objArray.push({ variety })
+    }
+  });
+
+  return objArray;
+}
+
+function normalizeWineTypesJSON (objJSON) {
+  const wineSet = new Set();
+  const wineTypeObj = {};
+  objJSON.forEach(({variety}, i) => {
+    if (wineSet.has(variety)) {
+
+    } else {
+      wineSet.add(variety);
+      wineTypeObj[variety] = {
+        variety,
+        index: i,
+      }
+    }
+  });
+
+  return wineTypeObj;
+}
+
+function makeWineJSON (objJSON, wineries, wineTypes) {
+  const wineObj = objJSON.map((obj, i) => {
+    const newObj = {
+      ...obj,
+      name: obj.title,
+      rating: obj.points,
+      userId: 1,
+      wineryId: wineries[obj.winery].index,
+      wineTypeId: wineTypes[obj.variety].index,
+      colorTypeId: 1,
+    };
+    delete newObj.title;
+    delete newObj.taster_name;
+    delete newObj.taster_twitter_handle;
+    delete newObj.variety;
+    delete newObj.points;
+    return newObj;
+  });
+
+  return wineObj;
+}
+
 const winesJSON = require('./wine-data-set.json');
 
-// const wines = JSON.parse(winesJSON);
 
-// console.log(typeof winesJSON);
-// console.log(winesJSON[0]);
+// normalize wine JSON data
+// const saveFile = './wine-data-normalized.json';
+// const normalizedWines = normalizeJSON(winesJSON);
+// writeJSONFile(normalizedWines, saveFile);
 
 
-// for (const [i, wine] of winesJSON.entries()) {
-  
-// }
 
-const wines = winesJSON.reduce((obj, entry, i) => {
-  console.log(i, entry);
-  obj[i] = entry;
-}, {});
+// normalize Wineries
+// const saveFile = './winery-normalized.json';
+// const wineriesNormalized = normalizeWineryJSON(winesJSON); // now normalizes
+// writeJSONFile(wineriesNormalized, saveFile);
 
-console.log(wines[0]);
+// make winery data
+// const saveFile = './winery-data.json';
+// const wineries = makeWineryJSON(winesJSON);
+// writeJSONFile(wineries, saveFile);
+
+
+// make Array of Objects for the Type of wines
+// const saveFile = './wine-types-data.json';
+// const wineTypes = makeWineTypesJSON(winesJSON);
+// writeJSONFile(wineTypes, saveFile);
+
+// normalize wine types
+// const saveFile = './wine-types-normalized.json';
+// const wineTypesNormalized = normalizeWineTypesJSON(winesJSON);
+// writeJSONFile(wineTypesNormalized, saveFile);
+
+
+
+
+// reformat array of wine objects
+// const saveFile = './wines-data.json';
+// const wines = makeWineJSON(winesJSON, wineriesNormalized, wineTypesNormalized);
+// writeJSONFile(wines, saveFile);
