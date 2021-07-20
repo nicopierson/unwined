919f120bb55cc44b7e062eef80935d2b620b4e69
Nico Pierson <nicogpt@gmail.com>
2021-07-19 23:15:49 -0700

    added api routes for wines

--- backend/routes/api/wines.js
+++ backend/routes/api/wines.js
@@ -9,6 +9,26 @@ const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 
+const wineNotFoundError = (id) => {
+  const err = Error("Wine not found");
+  if (id !== undefined) {
+    err.errors = [`Wine with id of ${id} could not be found.`];
+  } else {
+    err.errors = [`Wines could not be found.`];
+  }
+  err.title = "Wine not found.";
+  err.status = 404;
+  return err;
+};
+
+const winePostError = () => {
+  const err = Error("Wine could not be added");
+  err.errors = [`Wine could not be added`];
+  err.title = "Wine not added.";
+  err.status = 404;
+  return err;
+};
+
 const validateWine = [
   check('email')
     .exists({ checkFalsy: true })
@@ -29,21 +49,94 @@ const validateWine = [
   handleValidationErrors,
 ];
 
-router .get(
+router.get(
   '/',
-  asyncHandler(async (req, res) => {
+  asyncHandler(async (req, res, next) => {
     const wines = await Wine.findAll({ limit: 10 });
 
-    return res.json({
-      wines,
-    });
+    if (wines) {
+      return res.json({
+        wines,
+      });
+    } else {
+      next(wineNotFoundError());
+    }
+  })
+);
+
+router.get(
+  '/:id(\\id+)',
+  asyncHandler(async (req, res, next) => {
+    const wine = await Wine.findByPk(req.params.id);
+
+    if (wine) {
+      return res.json({
+        wine,
+      });
+    } else {
+      next(wineNotFoundError(req.params.id));
+    }
+  })
+);
+
+router.put(
+  '/:id',
+  requireAuth,
+  validateWine,
+  asyncHandler(async (req, res, next) => {
+    const {
+      name,
+      imageUrl,
+      description,
+      province,
+      country,
+      price,
+      rating,
+      designation,
+      region_1,
+      region_2,
+      userId,
+      wineryId,
+      colorTypeId,
+      wineTypeId,
+    } = req.body;
+
+    const wine = await Wine.findByPk(req.params.id);
+
+    if (wine) {
+      await wine.update({
+        name,
+        imageUrl,
+        description,
+        province,
+        country,
+        price,
+        rating,
+        designation,
+        region_1,
+        region_2,
+        userId,
+        wineryId,
+        colorTypeId,
+        wineTypeId,
+      });
+  
+      return res.json({
+        wine,
+      });
+
+    } else {
+      next(wineNotFoundError(req.params.id));
+    }
+
   })
 );
 
 router.post(
   '/',
+  requireAuth,
   validateWine,
-  asyncHandler(async (req, res) => {
+  asyncHandler(async (req, res, next) => {
     const {
       name,
       imageUrl,
@@ -84,16 +177,29 @@ router.post(
       return res.json({
         wine,
       });
+    } else {
+      next(winePostError());
     }
 
   }),
 );
 
-// router.delete(
-//   '/',
-//   (req, res) => {
-    
-//   }
-// );
+router.delete(
+  '/:id(\\d+)',
+  requireAuth,
+  asyncHandler(async (req, res, next) => {
+    const wine = await Wine.findByPk(req.params.id);
+
+    if (wine) {
+      await wine.destroy();
+
+      return res.json({
+        wine, // or the id => wine.id
+      });
+    } else {
+      next(wineNotFoundError(req.params.id));
+    }
+  }
+));
 
 module.exports = router;
