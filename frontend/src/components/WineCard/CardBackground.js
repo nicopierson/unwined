81c222d115d6cb68c99bc8ed0cbdfc39d11e725c
Nico Pierson <nicogpt@gmail.com>
2021-07-21 00:35:17 -0700

    finished WineCard component css and added wine bottle image

--- frontend/src/components/WineCard/CardBackground.js
+++ frontend/src/components/WineCard/CardBackground.js
@@ -1,14 +1,20 @@
 import styles from './CardBackground.module.css';
-import { ReactComponent as WineCardImg } from '../../assets/wine_platter.svg';
+// import { ReactComponent as WineCardImg } from '../../assets/wine_platter.svg';
+import redWineBottle from '../../assets/wine-bottle-transparent-red.png';
 
 const CardBackground = () => {
  return (
     <div className={styles.background}>
       <div className={styles.gradients}>
-        <div className={styles.gradient}></div>
+        <div className={styles.gradient} color='gray'></div>
       </div>
-      <h2 className={styles.logo}>Logo</h2>
-      <WineCardImg className={styles.background_img}/>
+      <div className={styles.logo}></div>
+      <h2 className={styles.logo_text}></h2>
+      <img alt='wine_bottle' src={redWineBottle} className={styles.background_img}/>
+      {/* <WineCardImg className={styles.background_img}/> */}
+      {/* <div className={styles.flag}>
+        <i className={`fa fa-flag`}></i>
+      </div> */}
     </div>
  );
 };
--- frontend/src/components/WineCard/CardInfo.js
+++ frontend/src/components/WineCard/CardInfo.js
@@ -4,23 +4,30 @@ const CardInfo = () => {
   return (
     <div className={styles.info}>
       <div className={styles.name}>
-        <div>
-          <h1>{`Stag's Leap Wine Cellars 1998 SLV Cabernet Sauvignon (Napa Valley)`}</h1>
+        <div className={styles.name_details}>
+          <h1 className={styles.text_big}>{`Stag's Leap Wine Cellars 1998 SLV Cabernet Sauvignon (Napa Valley)`}</h1>
           <span className={styles.country}>US</span>
         </div>
         <h3 className={styles.wine_type}>Cabernet Sauvignon</h3>
         <h3 className={styles.winery}>Stag's Leap Wine Cellars</h3>
       </div>
       <div className={styles.description}>
-        <h3 className={styles.description_info}>Description</h3>
+        <h3 className={`${styles.description_info} ${styles.title_info}`}>Description</h3>
         <p className={styles.description_text}>{'Certainly one of the successes of the vintage. Packed with cassis, sage, plum, tobacco, earth and smoky oak, and perfectly dry and balanced. Sheer joy in the mouth, like liquid velvet. It changes every second, offering up tiers of flavors through the spicy finish.'}</p>
       </div>
       <div className={styles.points_container}>
+        <span className={styles.title_info}>Rating</span>
         <h1 className={styles.points}>90</h1>
       </div>
-      <div className={styles.price}>
-        <i className={`fas fa-dollar-sign`}></i>
-        <h1>100</h1>.
+      <div className={styles.price_container}>
+        <span className={styles.favorite}>
+          <i className={`fab fa-gratipay`}></i>
+          Add to Favorites
+        </span>
+        <div className={styles.price}>
+          <i className={`fas fa-dollar-sign`}></i>
+          <h1>100</h1>
+        </div>
       </div>
     </div>
   );
