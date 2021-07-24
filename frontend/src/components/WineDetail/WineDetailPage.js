c7616bbd4ee3344b07072a28724c00e97f3d337a
Nico Pierson <nicogpt@gmail.com>
2021-07-24 13:46:25 -0700

    styled wine details

--- frontend/src/components/WineDetail/WineDetailPage.js
+++ frontend/src/components/WineDetail/WineDetailPage.js
@@ -16,7 +16,7 @@ const WineDetailPage = React.forwardRef(({ setToggleForm }, ref) => {
     history.push('/dashboard');
   } 
 
-  const winery = useSelector(state => state.wineries[wine.wineryId]);
+  const winery = useSelector(state => state.wineries[wine.wineryId] ? state.wineries[wine.wineryId] : 'unknown');
   const wineType = useSelector(state => state.wineTypes[wine.wineTypeId]);
 
   const handleDelete = async (event) => {
@@ -32,56 +32,65 @@ const WineDetailPage = React.forwardRef(({ setToggleForm }, ref) => {
   };
 
   return (
-      <>
-        <div className={styles.info}>
-          <div className={styles.name}>
-            <div className={styles.name_details}>
-              <h1 className={styles.text_big}>{wine?.name}</h1>
-            </div>
-            <div className={styles.card_sub_info}>
-              <h3 className={styles.wine_type}>{wineType?.variety}</h3>
-              <h3 className={styles.winery}>{winery?.name}</h3>
-              <span className={styles.country}>{wine?.country}</span>
+    <>
+      <div className={styles.info}>
+        <div className={styles.name}>
+          <div className={styles.name_details}>
+            <h1 className={styles.wine_name}>{wine.name}</h1>
+          </div>
+          <div className={styles.card_sub_info}>
+            <h3 className={styles.winery}>{winery.name ? winery.name : winery}</h3>
+            <h3 className={styles.wine_type}>{wineType.variety}</h3>
+            <div className={styles.country}>
+              <i className={`fas fa-globe-europe`}></i>
+              <span className={styles.country}>{wine.country}</span>
             </div>
           </div>
-          <div className={`${styles.description_container}`}>
-            <h3 className={`${styles.description_info} ${styles.title_info}`}>Description</h3>
-          </div>
+        </div>
+        <div className={`${styles.description_container}`}>
+          <h3 className={`${styles.description_info} ${styles.title_info}`}>Description</h3>
           <div className={styles.description}>
             <p className={styles.description_text}>{wine?.description}</p>
           </div>
+        </div>
+        <div className={styles.details_wrapper}>
           <div className={styles.rating_container}>
-            <span className={styles.title_info}>Rating</span>
             <div className={styles.rating}>
               <h1 className={styles.rating_text}>{wine.rating}</h1>
               <i className={`fas fa-star`}></i>
+              <span className={styles.title_info}>Rating</span>
             </div>
           </div>
-          <div className={styles.price_container}>
+          <div className={styles.favorite_container}>
             <span className={styles.favorite}>
               <i className={`fab fa-gratipay`}></i>
               Add to Favorites
             </span>
+          </div>
+          <div className={styles.price_container}>
             <div className={styles.price}>
               <i className={`fas fa-dollar-sign`}></i>
               <h1>{wine.price}</h1>
             </div>
           </div>
-        <button
-          onClick={handleEdit}
-        > 
-          Edit 
-        </button>
-        <button
-          onClick={() => history.push('/dashboard')}
-        > 
-          Back
-        </button>
-        <button
-          onClick={handleDelete}
-        > 
-          Delete
-        </button>
+        </div>
+        <div className={'styles.buttons'}>
+          <button
+            onClick={handleEdit}
+          > 
+            Edit 
+          </button>
+          <button
+            onClick={() => history.push('/dashboard')}
+          > 
+            Back
+          </button>
+          <button
+            onClick={handleDelete}
+          > 
+            Delete
+          </button>
+        </div>
       </div>
     </>
   );
