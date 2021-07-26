4139b4857ccf07c341a2660fb41d996b849f7517
Nico Pierson <nicogpt@gmail.com>
2021-07-25 21:58:57 -0700

    styled splash page

--- frontend/src/components/Splash/Splash.js
+++ frontend/src/components/Splash/Splash.js
@@ -4,6 +4,7 @@ import mainImgUrl from '../../assets/serge-esteve-unsplash.jpg';
 import unWinedLogo from '../../assets/unwined_logo_large.png';
 import findWineUrl from '../../assets/oscar-nord-unsplash.jpg';
 import checkInUrl from '../../assets/oscar-nord-unsplash.jpg';
+import reviewUrl from '../../assets/undraw_reviews.png';
 
 const Splash = () => {
 
@@ -14,7 +15,7 @@ const Splash = () => {
           <div className={styles.grid_wrapper}>
             <div className={styles.logo}>
               <img alt='unwined_logo' src={unWinedLogo} />
-              <h2>Discover and share your favorite wine.</h2>
+              <h2>Sit back and share your favorite wine</h2>
             </div>
             <img alt='main_wine_image' className={styles.wine_img} src={mainImgUrl} />
           </div>
@@ -24,7 +25,7 @@ const Splash = () => {
         <div className={styles.find_wine_container}>
           <img alt='find_wine_image' src={findWineUrl} />
           <div className={styles.find_wine_text}>
-            <h2>Discover new wines we think you’ll like.</h2>
+            <h2>Discover new wines we think you’ll like</h2>
             <p>Join us on the endless pursuit of great wine.</p>
           </div>
         </div>
@@ -32,10 +33,10 @@ const Splash = () => {
       <div className={styles.grid_wrapper_inline}>
         <div className={styles.check_ins_container}>
           <div className={styles.check_ins_text}>
-            <h2>Find recommendations from fellow wine connoisseurs.</h2>
+            <h2>Find recommendations from fellow wine connoisseurs</h2>
             <p>Learn about different types of wines from every part of the world.</p>
           </div>
-          <img alt='check_in_image' src={checkInUrl} />
+          <img alt='check_in_image' src={reviewUrl} />
         </div>
       </div>
       <div className={styles.footer_container}>
