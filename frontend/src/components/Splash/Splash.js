313d7b4b35a83be100ebe792cf6944bee99b3c42
Nico Pierson <nicogpt@gmail.com>
2021-07-25 19:21:30 -0700

    styled main image with logo on splash page

--- frontend/src/components/Splash/Splash.js
+++ frontend/src/components/Splash/Splash.js
@@ -1,6 +1,9 @@
 
 import styles from './Splash.module.css';
-import mainImgUrl from '../../assets/serge-esteve-unsplash.jpg'
+import mainImgUrl from '../../assets/serge-esteve-unsplash.jpg';
+import unWinedLogo from '../../assets/unwined_logo_large.png';
+import findWineUrl from '../../assets/oscar-nord-unsplash.jpg';
+import checkInUrl from '../../assets/oscar-nord-unsplash.jpg';
 
 const Splash = () => {
 
@@ -8,19 +11,24 @@ const Splash = () => {
     <div className={styles.splash_background}>
       <div className={styles.splash_main_container}>
         <div className={styles.splash_main}>
-          <div className={styles.splash_left}>
-            <img alt='main_wine_image' src={mainImgUrl} />
-          </div>
-          <div className={styles.splash_right}>
-
+          <div className={styles.grid_wrapper}>
+            <div className={styles.logo}>
+              <img alt='unwined_logo' src={unWinedLogo} />
+              <h2>Discover and share your favorite wine.</h2>
+            </div>
+            <img alt='main_wine_image' className={styles.wine_img} src={mainImgUrl} />
           </div>
         </div>
       </div>
-      <div className={styles.find_wine_container}>
-
+      <div className={styles.grid_wrapper_inline}>
+        <div className={styles.find_wine_container}>
+          <img alt='find_wine_image' src={findWineUrl} />
+        </div>
       </div>
-      <div className={styles.check_ins_container}>
-
+      <div className={styles.grid_wrapper_inline}>
+        <div className={styles.check_ins_container}>
+          <img alt='check_in_image' src={checkInUrl} />
+        </div>
       </div>
       <div className={styles.footer_container}>
 
